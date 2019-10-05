5711a3a3330437ea907cf9ae286cb00ec6b4ae9d
konrri <konrri@gmail.com>
2019-10-05 17:03:13 +0200

    rx Picker view in settings

--- HeatingClient/HeatingClient/Controllers/TempSettingVC.swift
+++ HeatingClient/HeatingClient/Controllers/TempSettingVC.swift
@@ -15,8 +15,9 @@ class TempSettingVC: UIViewController {
     static var id = "TempSettingVC"
     
     @IBOutlet weak var chartView: LineChartView!
-    @IBOutlet weak var steppersStackView: UIStackView!
+    @IBOutlet weak var pickerView: UIPickerView!
     
+    var pickerData: [[String]] = [[String]]()
     let disposeBag = DisposeBag()
     var settingVM: SettingsViewModel?
     var theThermostatVM: ThermostatViewModel?
@@ -27,32 +28,33 @@ class TempSettingVC: UIViewController {
         if chartView != nil {
             populateChart()
         }
-        setSeteppers()
+    
+        
+        //test Observable.just([[1.5, 2.4, 3.0], [5.3, 8.0, 13.0], [1.21, 2.34]])
+        Observable.just([buildRange(), buildRange(), buildRange(), buildRange()])
+            .bind(to: pickerView.rx.items(adapter: PickerViewViewAdapter()))
+            .disposed(by: disposeBag)
+        
+        pickerView.rx.modelSelected(Double.self)
+            .subscribe(onNext: { models in
+                logVerbose("picker choose: ")
+                print(models)
+                self.settingVM?.settings = models
+                self.redrawTheChart()
+            })
+            .disposed(by: disposeBag)
     }
     
-    func setSeteppers() {
-        if let stackView = steppersStackView {
-            stackView.alignment = UIStackView.Alignment.fill
-            
-            let dayLabel = UILabel()
-            dayLabel.text  = "  day   "
-
-            let nightLabel = UILabel()
-            nightLabel.text = "night  "
-            
-            let marginLabel = UILabel()
-            marginLabel.text = " ℃"
-            
-            //stackView.addArrangedSubview(dayLabel)
-            stackView.addArrangedSubview(buildStepper())
-            stackView.addArrangedSubview(buildStepper())
-            //stackView.addArrangedSubview(nightLabel)
-            stackView.addArrangedSubview(buildStepper())
-            stackView.addArrangedSubview(buildStepper())
-            //stackView.addArrangedSubview(marginLabel)
+    ///numbers that can be set
+    func buildRange() -> [Double] {
+        var ret = [Double]()
+        for i in stride(from: 29.0, to: 15.0, by: -0.5) {
+            ret.append(i)
         }
+        return ret
     }
     
+    @available(*, deprecated, message: "use pickerview")
     func buildStepper() -> UIStepper {
         let stepper = UIStepper()
         stepper.transform = CGAffineTransform.init(scaleX: 1.0, y: 0.6)
@@ -69,7 +71,8 @@ class TempSettingVC: UIViewController {
         stepper.rx.value.asObservable()
             .subscribe(onNext: {val in
                 log("stepper.rx.value subscribe val=\(val)")
-                self.settingVM?.settingDayAt6 = val
+                //self.settingVM?.settingDayAt6 = val
+                self.settingVM?.settings = [val, 0.0, 0.0, 0.0]
                 self.redrawTheChart()
             })
             .disposed(by: disposeBag)
@@ -107,3 +110,45 @@ class TempSettingVC: UIViewController {
         }
     }
 }
+
+
+final class PickerViewViewAdapter
+    : NSObject
+    , UIPickerViewDataSource
+    , UIPickerViewDelegate
+    , RxPickerViewDataSourceType
+, SectionedViewDataSourceType {
+    typealias Element = [[CustomStringConvertible]]
+    private var items: [[CustomStringConvertible]] = []
+    
+    func model(at indexPath: IndexPath) throws -> Any {
+        return items[indexPath.section][indexPath.row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return items.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return items[component].count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel()
+        label.text = items[component][row].description
+        label.textColor = UIColor.orange
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.textAlignment = .center
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, observedEvent: Event<Element>) {
+        Binder(self) { (adapter, items) in
+            adapter.items = items
+            pickerView.reloadAllComponents()
+            }.on(observedEvent)
+    }
+}
+
+
+
--- HeatingClient/HeatingClient/ViewModels/SettingsViewModel.swift
+++ HeatingClient/HeatingClient/ViewModels/SettingsViewModel.swift
@@ -17,7 +17,7 @@ class SettingsViewModel {
     var chartView: LineChartView?
     var roomName: String?
     
-    var settingDayAt6 = 0.0
+    var settings = [0.0, 0.0, 0.0, 0.0]
     
     init(_ rSetinngs: RoomsSettings) {
         self.roomsSettings = rSetinngs
@@ -84,11 +84,11 @@ class SettingsViewModel {
         let set1 = LineChartDataSet(entries: dayDataEntries, label: "old")
         let set2 = LineChartDataSet(entries: nightTempDataEntries, label: "old")
         
-        let setDay6cde = ChartDataEntry(x: 6.0, y: settingDayAt6)
-        let settDayEntries = [setDay6cde, day22cde]
+        let settDayEntries = [ChartDataEntry(x: 6.0, y: settings[0]), ChartDataEntry(x: 21.90, y: settings[1])]
+        let settNightEntries = [ChartDataEntry(x: 22.10, y: settings[2]), ChartDataEntry(x: 30.0, y: settings[3])]
         
         let set3 = LineChartDataSet(entries: settDayEntries, label: "new")
-        let set4 = LineChartDataSet(entries: nightTempDataEntries, label: "new")
+        let set4 = LineChartDataSet(entries: settNightEntries, label: "new")
         
         
         set1.lineWidth = 2.0
@@ -98,47 +98,5 @@ class SettingsViewModel {
         
         let data = LineChartData(dataSets: [set2,set1, set4, set3])
         chartView?.data = data
-    }
-    
-    @available(*, deprecated, message: "use build chart")
-    func chartData(for roomName: String, chartView: LineChartView) -> ([String]?, LineChartData?) {
-        var labels = Array(repeating: " ", count: 31)
-        labels[6] = "6:00"
-        labels[14] = "14:00"
-        labels[22] = "22:00"
-        labels[30] = "6:00"
-        
-        chartView.xAxis.axisMinimum = 5.0
-        chartView.xAxis.axisMaximum = 31.0
-        
-        chartView.leftAxis.axisMinimum = 14
-        chartView.leftAxis.axisMaximum = 26
-        chartView.rightAxis.enabled = false
-        
-        guard let roomS = roomsSettings.dict[roomName] else {
-            logError("there is no setting for room \(roomName)")
-            return (nil, nil)
-        }
-        
-        let day6cde = ChartDataEntry(x: 6.0, y: roomS.tempDay6 ?? 0.0)
-        let day22cde = ChartDataEntry(x: 21.90, y: roomS.tempDay22 ?? 0.0)
-        let night22cde = ChartDataEntry(x: 22.10, y: roomS.tempNight22 ?? 0.0)
-        let night6cde = ChartDataEntry(x: 30.0, y: roomS.tempNight6 ?? 0.0)
-        
-        let dayDataEntries = [day6cde, day22cde]
-        let nightTempDataEntries = [night22cde, night6cde]
-        
-        let set1 = LineChartDataSet(entries: dayDataEntries, label: "old")
-        let set2 = LineChartDataSet(entries: nightTempDataEntries, label: "old")
-        
-
-        set1.lineWidth = 2.0
-        set1.setColor(UIColor.green)
-        set2.setColor(.red)
-        set2.lineWidth = 2.0
-        
-        let data = LineChartData(dataSets: [set2,set1])
-        
-        return (labels, data)
     }
 }
